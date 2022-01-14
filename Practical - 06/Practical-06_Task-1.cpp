553a62bd83d5184590d96d8e196c295e7c691f52
satyamh1409 <73833500+satyamh1409@users.noreply.github.com>
2022-01-14 23:01:51 +0530

    Add files via upload

---
+++ Practical - 06/Practical-06_Task-1.cpp
@@ -0,0 +1,18 @@
+TASK 1
+//task1
+#include<iostream>
+#include<string>
+using namespace std ;
+int main ()
+{
+int a = 5 ;
+int b = 6 ;
+string s1 = "Hello", s2 = "World";
+int c = a+b ;
+int d = a+s1;
+string s = s1+s2;
+string t = a+s1;
+cout <<"When output was integer " << endl << c << endl << "When output was string "<< s <<endl;
+return 0 ;
+}
+
